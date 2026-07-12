7fc373d67e40a9128f508e3040631226d7871dd6
Praveen M <praveenm11111@gmail.com>
2026-07-12 15:57:21 +0530

    Add solution: Minimum Total Cost to Process All Elements (Medium) - LeetCode

--- Medium/minimum-total-cost-to-process-all-elements/Solution.java
+++ Medium/minimum-total-cost-to-process-all-elements/Solution.java
@@ -1,5 +1,24 @@
 class Solution {
     public int minimumCost(int[] nums, int k) {
-        return 6;
+        int available=k;
+        int totalcost=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]>available){
+                int totalAllocation=0;
+                totalAllocation+=(nums[i]/k);
+                if(nums[i]%k>1){
+                    totalAllocation++;
+                }
+                int temp=0;
+                while(temp<totalAllocation){
+                    totalcost+=temp;
+                    available+=k;
+                    temp++;
+                }
+            }
+            available-=nums[i];
+        }
+        return totalcost;
     }
 }
