d33bdfb123c12661e0bd53ba34db4ed73b32c51c
Praveen M <praveenm11111@gmail.com>
2026-07-16 23:19:23 +0530

    Add solution: Path Sum II (Medium) - LeetCode

--- Medium/path-sum-ii/Solution.java
+++ Medium/path-sum-ii/Solution.java
@@ -14,8 +14,8 @@
  * }
  */
 class Solution {
-    int sum=0;
-    public void helper(TreeNode root,int targetSum,List<Integer> currentpath,List<List<Integer>> paths){
+    
+    public void helper(TreeNode root,int targetSum,List<Integer> currentpath,int sum,List<List<Integer>> paths){
         if(root==null){
             return;
         }
@@ -27,10 +27,9 @@ class Solution {
             
             paths.add(new ArrayList<>(currentpath));
         }
-        helper(root.left,targetSum,currentpath,paths);
-        helper(root.right,targetSum,currentpath,paths);
+        helper(root.left,targetSum,currentpath,sum,paths);
+        helper(root.right,targetSum,currentpath,sum,paths);
         
-        sum-=root.val;
         currentpath.remove(currentpath.size()-1);
 
 
@@ -38,7 +37,7 @@ class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> paths=new ArrayList<>();
         List<Integer> currentpath=new ArrayList<>();
-        helper(root,targetSum,currentpath,paths);
+        helper(root,targetSum,currentpath,0,paths);
         return paths;
     }
 }
