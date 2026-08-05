51724b6dfd1d0cc1bf5d0f86a54892b694f49357
Praveen M <praveenm11111@gmail.com>
2026-08-05 23:01:38 +0530

    Add solution: Daily Temperatures (Medium) - LeetCode

--- Medium/daily-temperatures/Solution.java
+++ Medium/daily-temperatures/Solution.java
@@ -2,13 +2,13 @@ class Solution {
     public int[] dailyTemperatures(int[] nums) {
         int n=nums.length;
         int result[]=new int[n];
-        Deque<Integer> deque=new LinkedList<>();
+        Stack<Integer> deque=new Stack<>();
         for(int i=n-1;i>=0;i--){
-            while(!deque.isEmpty() && nums[deque.peekLast()]<=nums[i]){
-                deque.removeLast();
+            while(!deque.isEmpty() && nums[deque.peek()]<=nums[i]){
+                deque.pop();
             }
-            result[i]=deque.isEmpty()?0:deque.peekLast()-i;
-            deque.offerLast(i);
+            result[i]=deque.isEmpty()?0:deque.peek()-i;
+            deque.push(i);
         }
         return result;
     }
