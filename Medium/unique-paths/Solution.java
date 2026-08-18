eebdc73b2853e086308530b43a8dd206614b9748
Praveen M <praveenm11111@gmail.com>
2026-08-18 23:19:46 +0530

    Add solution: Unique Paths (Medium) - LeetCode

--- Medium/unique-paths/Solution.java
+++ Medium/unique-paths/Solution.java
@@ -2,27 +2,25 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m][n];
         
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    int up=0,right=0;
+                    if(i>0) 
+                        up=dp[i-1][j];
+                    if(j>0) 
+                        right=dp[i][j-1];
+                    dp[i][j]= up+right;
+                }
+            }
         }
-        helper(m-1,n-1,dp);
 
         return dp[m-1][n-1];
     }
 
-    public int helper(int row,int col,int[][]dp){
-        if(row==0 && col==0){
-            return dp[row][col]=1;
-        }
-        if(row<0 || col<0){
-            return 0;
-        }
-        if(dp[row][col]!=-1){
-            return dp[row][col];
-        }
-        int up=0,right=0;
-        up=helper(row-1,col,dp);
-        right=helper(row,col-1,dp);
-        return dp[row][col]= up+right;
-    }
+    
 }
