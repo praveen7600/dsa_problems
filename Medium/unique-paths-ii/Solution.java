94144b7a53ba44e45270f9e2e48455832f4f50bd
Praveen M <praveenm11111@gmail.com>
2026-08-19 23:01:10 +0530

    Add solution: Unique Paths II (Medium) - LeetCode

--- Medium/unique-paths-ii/Solution.java
+++ Medium/unique-paths-ii/Solution.java
@@ -5,12 +5,23 @@ class Solution {
         int n=mat[0].length;
         if(mat[0][0]==1) return 0;
         int [][]dp=new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        for(int row=0;row<m;row++){
+            for(int col=0;col<n;col++){
+                if(mat[row][col]==1){
+                    dp[row][col]=0;
+                }
+                else if(row==0 && col==0){
+                    dp[row][col]= 1;
+                }
+                else{
+                    int up=0,right=0;
+                    if(row>0) up=dp[row-1][col];
+                    if(col>0) right=dp[row][col-1];
+                    dp[row][col]= up+right;
+                }
+            }
         }
-        if(mat[0][0]!=1) 
-            dp[0][0]=1;
-        return helper(m-1,n-1,mat,dp);
+        return dp[m-1][n-1];
         
     }
     public int helper(int row,int col,int[][] mat,int[][] dp){
