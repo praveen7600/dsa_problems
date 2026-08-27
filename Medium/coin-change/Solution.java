5ab80c116d44d1478cec3200f021ed07f3d0b5bc
Praveen M <praveenm11111@gmail.com>
2026-08-27 22:57:06 +0530

    Add solution: Coin Change (Medium) - LeetCode

--- Medium/coin-change/Solution.java
+++ Medium/coin-change/Solution.java
@@ -2,31 +2,28 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][]dp=new int[n][amount+1];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+
+        for(int T=0;T<=amount;T++){
+            if(T%coins[0]==0){
+                dp[0][T]=T/coins[0];
+            }
+            else{
+                dp[0][T]=(int) 1e9;
+            }
         }
-        int mincoin= helper(coins,n-1,amount,dp);
-        return mincoin==1e9?-1:mincoin;
+        for(int ind=1;ind<n;ind++){
+            for(int target=0;target<=amount;target++){
+                int nottake=0+dp[ind-1][target];
+                int take=Integer.MAX_VALUE;
+                if(coins[ind]<=target){
+                    take=1+dp[ind][target-coins[ind]];
+                }
+                dp[ind][target]=Math.min(take,nottake);
+            }
+        }
+        int ans= dp[n-1][amount];
+        return ans>=1e9?-1:dp[n-1][amount]; 
         
     }
-    public int helper(int[] coins,int ind,int target,int[][] dp){
-
-        if(ind==0){
-            if(target%coins[ind]==0){
-                return target/coins[ind];
-            }
-            return (int)1e9;
-        }
-
-        if(dp[ind][target]!=-1){
-            return dp[ind][target];
-        }
-
-        int nottake=0+helper(coins,ind-1,target,dp);
-        int take=Integer.MAX_VALUE;
-        if(coins[ind]<=target){
-            take=1+helper(coins,ind,target-coins[ind],dp);
-        }
-        return dp[ind][target]=Math.min(take,nottake);
-    }
+    
 }
