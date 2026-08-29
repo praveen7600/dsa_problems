91a5a0b36adb6ad1c3a7781d4989c0012952a2a2
Praveen M <praveenm11111@gmail.com>
2026-08-30 00:46:29 +0530

    Add solution: Minimum Bishop Moves to Reach Target (Medium) - LeetCode

--- Medium/minimum-bishop-moves-to-reach-target/Solution.java
+++ Medium/minimum-bishop-moves-to-reach-target/Solution.java
@@ -6,53 +6,13 @@ class Solution {
         int tr=target[0]-1;
         int tc=target[1]-1;
 
-        if((((sr+sc)%2==0) && ((tr+tc)%2!=0)) || (((sr+sc)%2!=0) && ((tr+tc)%2==0)) ){
+        if( (sr+sc)%2 !=(tr+tc)%2 ){
             return -1;
         }
 
-        if(leftup(sr,sc,tr,tc) || leftbottom(sr,sc,tr,tc) || rightup(sr,sc,tr,tc) || rightbottom(sr,sc,tr,tc)){
+        if(Math.abs(sr-tr)==Math.abs(sc-tc)){
             return 1;
         }
         return 2;
-    }
-    public boolean leftup(int sr,int sc,int tr,int tc){
-        while(sr>0 && sc>0){
-            sr-=1;
-            sc-=1;
-            if(sr==tr && sc==tc){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean leftbottom(int sr,int sc,int tr,int tc){
-        while(sr<n && sc>0){
-            sr+=1;
-            sc-=1;
-            if(sr==tr && sc==tc){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean rightup(int sr,int sc,int tr,int tc){
-        while(sr>0 && sc<n){
-            sr-=1;
-            sc+=1;
-            if(sr==tr && sc==tc){
-                return true;
-            }
-        }
-        return false;
-    }
-    public boolean rightbottom(int sr,int sc,int tr,int tc){
-        while(sr<n && sc<n){
-            sr+=1;
-            sc+=1;
-            if(sr==tr && sc==tc){
-                return true;
-            }
-        }
-        return false;
     }
 }
