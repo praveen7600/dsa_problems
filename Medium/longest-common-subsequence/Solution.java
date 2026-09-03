743d08ca1c59eeb6c24d279bfce17ce2a6cdc858
Praveen M <praveenm11111@gmail.com>
2026-09-03 22:13:46 +0530

    Add solution: Longest Common Subsequence (Medium) - LeetCode

--- Medium/longest-common-subsequence/Solution.java
+++ Medium/longest-common-subsequence/Solution.java
@@ -1,25 +1,40 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int n=text1.length();
-        int m=text2.length();
-        int[][] dp=new int[n][m];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
-        }
-        return LCS(text1,text2,n-1,m-1, dp);
-    }
-    public int LCS(String s1,String s2,int ind1,int ind2,int[][] dp){
-        if(ind1<0 || ind2<0){
-            return 0;
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+            }
         }
 
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
+        for(int ind1=1;ind1<n+1;ind1++){
+            for(int ind2=1;ind2<m+1;ind2++){
+                if(s1.charAt(ind1-1)==s2.charAt(ind2-1)){
+                    dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+                }
+                else{
+                    dp[ind1][ind2]=Math.max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                }
+            }
         }
-
-        if(s1.charAt(ind1)==s2.charAt(ind2)){
-            return dp[ind1][ind2]=1+LCS(s1,s2,ind1-1,ind2-1,dp);
-        }
-        return dp[ind1][ind2]=Math.max(LCS(s1,s2,ind1-1,ind2,dp),LCS(s1,s2,ind1,ind2-1,dp));
+        return dp[n][m];
     }
+    // public int LCS(String s1,String s2,int ind1,int ind2,int[][] dp){
+    //     if(ind1==0 || ind2==0){
+    //         return 0;
+    //     }
+
+    //     if(dp[ind1][ind2]!=-1){
+    //         return dp[ind1][ind2];
+    //     }
+
+    //     if(s1.charAt(ind1-1)==s2.charAt(ind2-1)){
+    //         return dp[ind1][ind2]=1+LCS(s1,s2,ind1-1,ind2-1,dp);
+    //     }
+    //     return dp[ind1][ind2]=Math.max(LCS(s1,s2,ind1-1,ind2,dp),LCS(s1,s2,ind1,ind2-1,dp));
+    // }
 }
