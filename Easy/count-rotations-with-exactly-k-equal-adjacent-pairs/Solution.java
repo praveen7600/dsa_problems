740dc74343af62e9b9df184ebe516b143c39f684
Praveen M <praveenm11111@gmail.com>
2026-09-07 23:28:54 +0530

    Add solution: Count Rotations With Exactly K Equal Adjacent Pairs (Easy) - LeetCode

--- Easy/count-rotations-with-exactly-k-equal-adjacent-pairs/Solution.java
+++ Easy/count-rotations-with-exactly-k-equal-adjacent-pairs/Solution.java
@@ -1,25 +1,32 @@
 class Solution {
     public int countRotations(String s, int k) {
         int n=s.length();
-        int result=0;
-        for(int i=0;i<s.length();i++){
-            StringBuilder str=new StringBuilder();
-            str.append(s.substring(i,n)).append(s.substring(0,i));
-            if(helper(str.toString())==k){
-                result++;
-            }
-        }
-        return result;
-    }
 
-    public int helper(String s){
-        int n=s.length();
         int adjcount=0;
         for(int i=1;i<n;i++){
             if(s.charAt(i)==s.charAt(i-1)){
                 adjcount++;
             }
         }
-        return adjcount;
+        
+        
+        if(s.charAt(n-1)==s.charAt(0)){
+            adjcount++;
+        }
+        
+        if(k==adjcount){
+            return n-adjcount;
+        }
+
+        if(k==adjcount-1){
+            return adjcount;
+        }
+        
+
+        return 0;
+
+       
     }
+
+    
 }
