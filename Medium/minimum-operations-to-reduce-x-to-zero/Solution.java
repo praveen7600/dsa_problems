43e26a1a1d1d4437169915795a14c2678f32232b
Praveen M <praveenm11111@gmail.com>
2026-09-23 22:47:34 +0530

    Add solution: Minimum Operations to Reduce X to Zero (Medium) - LeetCode

--- Medium/minimum-operations-to-reduce-x-to-zero/Solution.java
+++ Medium/minimum-operations-to-reduce-x-to-zero/Solution.java
@@ -1,28 +1,38 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int total=0;
+        int n=nums.length;
+        int sum=0;
         for(int i:nums){
-            total+=i;
+            sum+=i;
         }
-        if(total-x<0) return -1;
-        int maxlen= longestSubarray(nums,total-x);
-        if(maxlen==-1) return -1;
-        return nums.length-maxlen;
+        if(sum-x<0){
+            return -1;
+        }
+        if(sum-x==0){
+            return n;
+        }
+        int maxlen=maxSubarrayLength(nums,sum-x);
+        if(maxlen==-1){
+            return -1;
+        }
+        return n-maxlen;
     }
-    public int longestSubarray(int nums[],int k){
-        int left=0,right=0,n=nums.length;
-        int sum=0,maxlen=-1;
+    public int maxSubarrayLength(int[] arr,int sum){
+        int maxlen=-1;
+        int left=0,right=0;
+        int cursum=0, n=arr.length;
         while(right<n){
-            sum+=nums[right];
-            while(sum>k){
-                sum-=nums[left];
+            cursum+=arr[right];
+            while(cursum>sum){
+                cursum-=arr[left];
                 left++;
             }
-            if(sum==k){
-                maxlen=Math.max(maxlen,right-left+1);
+            if(cursum==sum){
+                maxlen=Math.max(right-left+1,maxlen);
             }
             right++;
         }
+        
         return maxlen;
     }
 }
